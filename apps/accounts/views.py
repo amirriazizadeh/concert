75c83d50a9faab9d6a8f95be07bb7bc57107d675
amirriazizadeh <ariazizadeh@gmail.com>
2024-05-24 12:59:28 +0330

    adding: login and logout has been added

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from apps import ticketSales
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+def loginView(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user=user)
+            if request.GET.get('next'):
+                return redirect(request.GET.get('next'))
+            return redirect(reverse(ticketSales.views.timesInfo))
+        else:
+            context = {
+                "user": username,
+                "error": "کاربر با این نام یافت نشد..."
+            }
+            return render(request, 'accounts/login.html', context)
+    else:
+        return render(request, 'accounts/login.html', {})
+
+def logoutView(request):
+    logout(request)
+    if request.GET.get("next"):
+        return render(request.GET.get("next"))
+    return redirect(reverse(ticketSales.views.concertListView))
+    
--- concert/settings.py
+++ concert/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static/'),)
 
 # Default primary key field type
