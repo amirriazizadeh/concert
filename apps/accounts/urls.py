b0c0b90484e4f867a1e8df6e50b2a15fca7a6afc
amirriazi <ariazizadeh@gmail.com>
2024-06-12 14:22:19 +0330

    before CBV version

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import loginView,logoutView,profileView
+from .views import loginView,logoutView,profileView,profileRegisterView
 
 urlpatterns=[
     path("login/",loginView,name="login"),
     path("logout/",logoutView,name="logout"),
     path("profile/",profileView,name="profile"),
+    path("profileRegister/",profileRegisterView,name="reg"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from .forms import ProfileRegisterForm
+from .models import ProfileModel
 
 def loginView(request):
     if request.method == "POST":
@@ -39,3 +40,37 @@ def profileView(request):
         "profile":profile
     }
     return render(request,"accounts/profile.html",context)
+
+def profileRegisterView(request):
+
+    if request.method=="POST":
+        profileRegisterForm=ProfileRegisterForm(request.POST,request.FILES)
+        if profileRegisterForm.is_valid():
+
+            user = User.objects.create_user(
+                                username=profileRegisterForm.cleaned_data["username"],
+                                email=profileRegisterForm.cleaned_data['email'],
+                                password=profileRegisterForm.cleaned_data['password'],
+                                first_name=profileRegisterForm.cleaned_data['first_name'],
+                                last_name=profileRegisterForm.cleaned_data['last_name'])
+
+            user.save()
+
+            profileModel=ProfileModel(user=user,
+                                       ProfileImage=profileRegisterForm.cleaned_data['ProfileImage'],
+                                        Gender=profileRegisterForm.cleaned_data['Gender'],
+                                        Credit=profileRegisterForm.cleaned_data['Credit'])
+
+            profileModel.save()
+
+            return HttpResponseRedirect(reverse(ticketSales.views.concertListView))
+    else:
+        profileRegisterForm=ProfileRegisterForm()
+
+  
+    context={
+        "formData":profileRegisterForm
+    }
+
+
+    return render(request,"accounts/profileRegister.html",context)
